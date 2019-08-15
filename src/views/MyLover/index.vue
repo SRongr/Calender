86fdc3f1d356c44ad0a291796bb51c32972186f4
wangrongshuo <wrs970808@163.com>
2019-08-15 16:30:56 +0800

    ceshi

---
+++ src/views/MyLover/index.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+
+</template>
+
+<script>
+import Vue from 'vue'
+import 'swiper/dist/css/swiper.css'
+
+import images from './images.json'
+if (process.browser) {
+  const VueAwesomeSwiper = require('vue-awesome-swiper/dist/ssr')
+  Vue.use(VueAwesomeSwiper)
+}
+export default {
+  name: 'Lover',
+  components: {
+  
+  },
+  data() {
+    return {
+     ceshi:'ceshi'
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="stylus">
+@import './common/css/main.styl'
+
+</style>
